d1b67b59e7b3344ea988f5d9199402cd9cae9dc2
Eduardo Duarte Derisso <dudu.derisso@gmail.com>
2024-01-02 11:55:31 -0300

    aula 4: utilizar componentes para Grid Layout (SimpleGrig, Grid, GridItem)

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,27 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/navbar"
+import { Grid, GridItem } from "@chakra-ui/react"
 
 export default function RootLayout() {
   return (
-    <div>
-      <Navbar/>
-      <Outlet />
-    </div>
+    <Grid templateColumns="repeat(6, 1fr)"
+    bg="gray.200">
+      <GridItem
+        as="aside"
+        colSpan="1"
+        bg="purple.400"
+        minHeight="1000hv"
+        p="30px"
+      >
+
+        <span>sidebar</span>
+
+      </GridItem>
+      
+      <GridItem colSpan="5">
+        <Navbar/>
+        <Outlet />
+      </GridItem>
+    </Grid>
   )
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,34 +1,32 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 
 export default function Dashboard() {
 
-  const boxStyles = {
-    p: "10px",
-    bg: "purple.400",
-    color: "white",
-    m: "10px",
-    taextAlign: "center",
-    filter: "blur(2px)",
-    ':hover': {
-      color: "black",
-      bg: "blue.200"
-    }
-  }
 
   return (
-    <Container as="section" maxWidth="4xl" py="20px">
-      <Heading my="30px" p="10px">Heading</Heading>
-      <Text marginLeft="30px"> dsfasd sdfsa sdfa sfafasfasfasfdsa</Text>
-      <Text marginLeft="30px" color="blue.300" fontWeight="bold"> dsfasd sdfsa sdfa sfafasfasfasfdsa</Text>
+    <SimpleGrid p="20px" columns="4" spacing="10px" minChildWidth="250px">
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
 
-      <Box my="30px" p="20px" bg="orange">
-        <Text color="white">This is a Box</Text>
-      </Box>
 
-      <Box sx={boxStyles}>
-        Hello, Ninjas
-      </Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
 
-    </Container>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+
+
+    </SimpleGrid>
   )
 }
